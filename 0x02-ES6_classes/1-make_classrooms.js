59e733eee9b367fe08bd3b25bab43841058235e3
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-05-31 20:59:41 +0100

    task 1

--- 0x02-ES6_classes/1-make_classrooms.js
+++ 0x02-ES6_classes/1-make_classrooms.js
@@ -2,9 +2,9 @@
 import ClassRoom from './0-classroom';
 
 export default function initializeRooms() {
-  const room = new ClassRoom(19);
-  const room1 = new ClassRoom(20);
-  const room2 = new ClassRoom(34);
+  room = new ClassRoom(19);
+  room1 = new ClassRoom(20);
+  room2 = new ClassRoom(34);
 
   return [room, room1, room2];
 }
