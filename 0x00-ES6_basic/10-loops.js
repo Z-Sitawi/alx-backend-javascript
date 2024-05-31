1cecbc181517484f6581ac07212b5076cb9f9166
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-05-31 21:34:06 +0100

    task 10

--- 0x00-ES6_basic/10-loops.js
+++ 0x00-ES6_basic/10-loops.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+export default function appendToEachArrayValue(array, appendString) {
+  for (let idx of array) {
+      let value = idx;
+      array.push(appendString + value);
+  }
+  return array;
+}
