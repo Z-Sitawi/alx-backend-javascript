c5d2b53ee961048106b553e36b7f20fd12946527
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-05-31 20:55:59 +0100

    task 4

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,1 +1,32 @@
 #!/usr/bin/node
+export default class Pricing {
+  /* eslint-disable no-underscore-dangle */
+  constructor(amount, currency) {
+    this._amount = amount;
+    this._currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this._currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    return amount * conversionRate;
+  }
+}
