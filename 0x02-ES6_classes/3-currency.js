2d951b34a524539457ed8556ebf2fc5c195ce082
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-05-31 20:23:10 +0100

    task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -1,1 +1,28 @@
 #!/usr/bin/node
+export default class Currency {
+  /* eslint-disable no-underscore-dangle */
+  constructor(code, name) {
+    this._code = code;
+    this._name = name;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set name(newVal) {
+    this._name = newVal;
+  }
+
+  set code(newVal) {
+    this._code = newVal;
+  }
+
+  displayFullCurrency() {
+    return `${this._name} (${this._code})`;
+  }
+}
