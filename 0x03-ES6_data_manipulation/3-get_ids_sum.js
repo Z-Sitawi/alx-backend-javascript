c840e817ea37ea44c32038d3a098abdc59c8146b
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-04 21:25:23 +0100

    task 3

--- 0x03-ES6_data_manipulation/3-get_ids_sum.js
+++ 0x03-ES6_data_manipulation/3-get_ids_sum.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+function getSum(num1, num2) {
+  return num1 + num2;
+}
+
+export default function getStudentIdsSum(objsArray) {
+  const studentsIdsList = objsArray.map((elelment) => elelment.id);
+  return studentsIdsList.reduce(getSum, 0);
+}
