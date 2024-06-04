5ad2734083fcb91ac8eb4e778ac229051a7be879
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-04 22:59:41 +0100

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,1 +1,10 @@
 #!/usr/bin/node
+export default function cleanSet(Set, myStartString) {
+  if (!Set || !myStartString || !(Set instanceof Set) || typeof myStartString !== 'string') {
+  return '';
+  }
+  return Array.from(Set)
+    .filter((ele) => ele && ele.startsWith(myStartString))
+    .map((ele) => ele.replace(myStartString, ''))
+    .join('-');
+}
