319e14276a96e71b5e2fa5e369d102e444937cc6
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-04 22:21:01 +0100

    tasjk

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,1 +1,11 @@
 #!/usr/bin/node
+export default function createInt8TypedArray(length, position, value) {
+  if (position < 0 || position >= length) {
+    throw new Error('Position outside range');
+  }
+  const buffer = new ArrayBuffer(length);
+  const int8Array = new Int8Array(buffer);
+  int8Array[position] = value;
+
+  return buffer;
+}
