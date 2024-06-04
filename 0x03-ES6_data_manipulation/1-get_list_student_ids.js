34cd8d14f577a1563a5db379eb2bd543fbd1b047
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-04 19:24:23 +0100

    task 1

--- 0x03-ES6_data_manipulation/1-get_list_student_ids.js
+++ 0x03-ES6_data_manipulation/1-get_list_student_ids.js
@@ -1,18 +1,4 @@
 #!/usr/bin/node
-function isArrayOfObjects(arr) {
-  if (!Array.isArray(arr)) {
-    return false;
-  }
-  return arr.every((item) => typeof item === 'object' && item !== null);
-}
-
-export default function getListStudentIds(objsArray) {
-  if (isArrayOfObjects(objsArray)) {
-    const listIds = [];
-    objsArray.forEach((element) => {
-      listIds.push(element.id);
-    });
-    return listIds;
-  }
-  return [];
+export default function getListStudentIds(xlistStudents) {
+  return Array.isArray(xlistStudents) ? xlistStudents.map(({ id }) => id) : [];
 }
