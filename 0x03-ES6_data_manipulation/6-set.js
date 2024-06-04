dc3883d316abec2808ca0eadb2fdf5b09cf23dae
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-04 22:54:55 +0100

    task 6 and 7

--- 0x03-ES6_data_manipulation/6-set.js
+++ 0x03-ES6_data_manipulation/6-set.js
@@ -1,1 +1,4 @@
 #!/usr/bin/node
+export default function setFromArray(myArray) {
+  return new Set([...myArray]);
+}
--- 0x03-ES6_data_manipulation/7-has_array_values.js
+++ 0x03-ES6_data_manipulation/7-has_array_values.js
@@ -1,1 +1,4 @@
 #!/usr/bin/node
+export default function hasValuesFromArray(Set, myArray) {
+  return myArray.every((ele) => Set.has(ele));
+}
