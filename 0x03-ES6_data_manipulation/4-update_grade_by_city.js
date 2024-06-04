6e2f153c03b3ed8ba26692f7bff669eb5700933a
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-06-04 22:07:25 +0100

    task 4

--- 0x03-ES6_data_manipulation/4-update_grade_by_city.js
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -1,1 +1,15 @@
 #!/usr/bin/node
+/* eslint-disable no-param-reassign */
+export default function updateStudentGradeByCity(objsArray, city, newGrades) {
+  return objsArray
+    .filter((element) => element.location === city)
+    .map((obj) => {
+      const newGradesList = newGrades.filter(({ studentId }) => studentId === obj.id);
+      if (newGradesList.length > 0) { // Check if newGradesList exists and has elements
+        obj.grade = newGradesList[0].grade;
+      } else {
+        obj.grade = 'N/A';
+      }
+      return obj; // Return the updated object
+    });
+}
