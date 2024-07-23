e6d64f8aa0260bc1d86362bcbc0f37f6b03ed953
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-23 23:54:16 +0100

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,1 +1,41 @@
 #!/usr/bin/node
+const fs = require('fs');
+
+function countStudents(path) {
+  try {
+    const data = fs.readFile(path, 'utf8').trim();
+
+    // Split the data into lines and parse each line
+    const lines = data.split('\n');
+    lines.shift();
+    const students = [];
+    lines.forEach((line) => {
+      const row = line.split(',');
+      const student = {
+        firstname: row[0],
+        lastname: row[1],
+        age: row[2],
+        field: row[3],
+      };
+      students.push(student);
+    });
+    const allFields = students.map((student) => student.field);
+    const distinctFields = [...new Set(allFields)];
+
+    console.log(`Number of students: ${students.length}`);
+
+    distinctFields.forEach((ele) => {
+      const fStudents = students.filter((student) => student.field === ele);
+      let fSnames = '';
+      for (let i = 0; i < fStudents.length; i += 1) {
+        if (i < fStudents.length - 1) fSnames += `${fStudents[i].firstname}, `;
+        else fSnames += fStudents[i].firstname;
+      }
+      console.log(`Number of students in ${ele}: ${fStudents.length}. List: ${fSnames}`);
+    });
+  } catch (error) {
+    throw new Error('Cannot load the database');
+  }
+}
+
+module.exports = countStudents;
