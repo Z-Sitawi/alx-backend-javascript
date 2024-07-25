9ac27e9abc812c198439927685ab579e8678c0b6
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-25 23:13:01 +0100

    task 3

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,1 +1,9 @@
 #!/usr/bin/node
+const utils = require('./utils.js');
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+  const total = utils.calculateNumber('SUM', totalAmount, totalShipping);
+  console.log(`The total is: ${total}`);
+}
+
+module.exports = sendPaymentRequestToApi;
