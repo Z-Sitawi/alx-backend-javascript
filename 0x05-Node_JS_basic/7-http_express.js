02e23c20b9baff7ff47d81cdcec9faac9263fca8
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-27 00:30:39 +0100

    task 7

--- 0x05-Node_JS_basic/7-http_express.js
+++ 0x05-Node_JS_basic/7-http_express.js
@@ -3,39 +3,32 @@ const express = require('express');
 const fs = require('fs').promises;
 
 const app = express();
-const PORT = 1245;
 
 function countStudents(path) {
   return new Promise((resolve, reject) => {
     fs.readFile(path, 'utf8')
       .then((data) => {
-        const lines = data.trim().split('\n');
-        if (lines.length === 0) {
-          return reject(new Error('File is empty'));
-        }
-
+        const lines = data.split('\n');
         const hashtable = {};
-        let students = 0;
+        let students = -1;
         let result = '';
-
-        // Assuming the first line is the header
-        for (let i = 1; i < lines.length; i++) {
-          const line = lines[i].trim();
-          if (line !== '') {
+        for (const line of lines) {
+          if (line.trim() !== '') {
             const columns = line.split(',');
             const field = columns[3];
             const firstname = columns[0];
-            if (!hashtable[field]) {
-              hashtable[field] = [];
+            if (students >= 0) {
+              if (!Object.hasOwnProperty.call(hashtable, field)) {
+                hashtable[field] = [];
+              }
+              hashtable[field] = [...hashtable[field], firstname];
             }
-            hashtable[field].push(firstname);
             students += 1;
           }
         }
-
         result += `Number of students: ${students}\n`;
         for (const key in hashtable) {
-          if (Object.prototype.hasOwnProperty.call(hashtable, key)) {
+          if (Object.hasOwnProperty.call(hashtable, key)) {
             result += `Number of students in ${key}: ${hashtable[key].length}. List: ${hashtable[key].join(', ')}\n`;
           }
         }
@@ -48,25 +41,19 @@ function countStudents(path) {
 }
 
 app.get('/', (req, res) => {
-  res.status(200).send('Hello Holberton School!');
+  res.send('Hello Holberton School!');
 });
 
 app.get('/students', (req, res) => {
   countStudents(process.argv[2])
     .then((data) => {
-      res.status(200).send(`This is the list of our students\n${data}`);
+      res.send(`This is the list of our students\n${data}`);
     })
     .catch((error) => {
       res.status(500).send(`This is the list of our students\n${error.message}`);
     });
 });
 
-app.use((req, res) => {
-  res.status(404).send('Not found');
-});
-
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+app.listen(1245);
 
 module.exports = app;
