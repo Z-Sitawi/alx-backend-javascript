df6141f1080cf6c0ba97cfb89252d2a5b2a7d848
Z-sitawi <aaichaou.zakaria@gmail.com>
2024-07-26 21:18:58 +0100

    task 4

--- 0x05-Node_JS_basic/4-http.js
+++ 0x05-Node_JS_basic/4-http.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 const http = require('http');
 
-const app = http.createServer(function (req, res) {
+const app = http.createServer((req, res) => {
   res.write('Hello Holberton School!');
   res.end();
 }).listen(1245);
